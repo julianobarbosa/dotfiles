7ff0d122072833a71883387e86641b4b8a6fbb9f
Juliano Barbosa <julianomb@gmail.com>
2019-06-19 10:42:30 -0300

    Add EDITOR window

--- .local/bin/tmux_init.sh
+++ .local/bin/tmux_init.sh
@@ -6,6 +6,13 @@ tmux selectp -t 1
 tmux send-keys "cd $HOME" C-m
 tmux send-keys "nvim" C-m
 
+# Split panel 1, set dir to Project
+tmux splitw -h -p 50 -c '$HOME/__Project/Envs'
+tmux rename-windows SERVER
+tmux send-keys 'cd $HOME/__Projects/Env' C-m
+tmux splitw -v -p 75 -c '$HOME/__Project/Envs'
+tmux rename-windows SHELL
+
 # create a new window called DOCKER
 tmux new-window -c '$HOME' -t $session:5 -n CHAT
 tmux send-keys "/usr/bin/weechat" C-m
