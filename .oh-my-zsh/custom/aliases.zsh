d3f180186fe9b2c104b55744df2d49a1254c3ba5
Juliano Barbosa <julianomb@gmail.com>
2019-09-02 17:03:40 -0300

    Add Docker aliases

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -47,8 +47,13 @@ alias quit=q
 # KILL
 alias kill_port="fuser -n tcp -k $1"
 
+# nvim
+alias e='nvim $1'
+alias v='nvim $1'
+alias vi='nvim $1'
+alias vim='nvim $1'
+
 # TMUX
-alias v='vim $1'
 alias ta='tmux -2 attach -t'
 alias tl='tmux -2 list-session'
 alias tn='tmux -2 new -s $USER'
@@ -65,4 +70,30 @@ alias ohmyzsh="vim ~/.oh-my-zsh"
 
 # Docker
 alias diclean='docker images | grep '\''\'' | grep -P '\''[1234567890abcdef]{12}'\'' -o | xargs -L1 docker rmi'
-#
+alias dk="docker"
+alias dkps="docker ps"
+alias dki="docker inspect"
+alias dkr="docker restart"
+alias dkl="docker logs --tail=200 -f"
+alias dkc="docker-compose"
+alias dkcl="docker-compose logs --tail=200 -f"
+alias dkcr="docker-compose restart"
+alias dkm="docker-machine"
+function dkb() { docker exec -it $1 script -q -c "TERM=xterm /bin/bash" /dev/null; }
+function dkrb() { docker run --rm -it -v /tmp:/tmp/host ${1:-"ubuntu:18.04"} script -q -c "TERM=xterm /bin/bash" /dev/null; }
+function dkjl() { journalctl -b CONTAINER_NAME=$1 -e ${@:2}; }
+function dkip() { docker inspect $1 | jq -r .[0].NetworkSettings.IPAddress; }
+function docker-clean() {
+  docker rm -v $(docker ps --filter status=exited -q 2>/dev/null) 2>/dev/null
+  docker rmi $(docker images --filter dangling=true -q 2>/dev/null) 2>/dev/null
+  docker volume rm $(docker volume ls -qf dangling=true 2>/dev/null) 2>/dev/null
+}
+
+## Kubernetes
+alias kb="kubectl"
+
+## Ansible
+alias asb="ansible -s -i inventory -m shell -a"
+alias asbp="ansible -s -i inventories/ -m shell -a"
+alias apb="ansible-playbook -i inventory playbook.yml"
+alias apbp="ansible-playbook -i inventories/ playbook.yml"
