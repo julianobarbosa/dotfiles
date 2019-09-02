a8c4de2911e04826b2833e179d8aa9dbf8f886bc
Juliano Barbosa <julianomb@gmail.com>
2019-09-02 19:08:13 -0300

    Change tmux_init.sh

--- bin/tmux_init.sh
+++ bin/tmux_init.sh
@@ -1,2 +1,20 @@
-#/bin/bash
-tmux attach -t base || tmux new -s base
+#/usr/bin/zsh
+tmux attach -t base || tmux new -c '$HOME' -s base -n EDITOR
+
+# Select pane 1, set dir to api, run vim
+tmux selectp -t 1
+tmux send-keys "cd $HOME" C-m
+# tmux send-keys "nvim" C-m
+
+# Split panel 1, set dir to Project
+tmux splitw -h -p 50 -c '$HOME/__Project/Envs'
+tmux rename-windows SERVER
+tmux send-keys 'cd $HOME/__Projects/Env' C-m
+tmux splitw -v -p 75 -c '$HOME/__Project/Envs'
+tmux rename-windows SHELL
+
+# create a new window called DOCKER
+tmux new-window -c '$HOME' -t $session:5 -n CHAT
+tmux send-keys "pyenv activate weechat" C-m
+tmux send-keys "/usr/bin/weechat" C-m
+
